adf935be6b40022f53021c2a0a7871b1ecf541d8
RaechelRM-10 <raechelmarierola@gmail.com>
2024-09-13 21:28:46 +0800

    nodemon

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.Db_Name, process.env.Db_User , '',{
-    host: process.env.Db_HOST,
-    port: process.env.Db_PORT,
-    dialect: 'mssql',
-    dialectOptions: {
-        options:{
-            trustedConnection: true,
-            encrypt: true,
-            trustServerCertificate: true, 
-        },
+const sequelize = new Sequelize(process.env.Db_Name, process.env.Db_User, process.env.Db_Password, {
+  host: process.env.Db_HOST,
+  port: process.env.Db_PORT || 1433, // Default to 1433 if not specified
+  dialect: 'mssql',
+  dialectOptions: {
+    options: {
+      encrypt: true, // For Azure SQL
+      trustServerCertificate: true, // Disable in production
     },
-    logging: console.log
+  },
+  logging: console.log,
 });
 
 module.exports = sequelize;
